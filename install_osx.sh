83ab3b92a0cbedf79d37c7de5d0d8e889fc1b5f0
Daniel Gen Li <dgli@Daniels-iMac.local>
2016-03-07 14:29:10 -0500

    made a small install script.  don't know why I didn;t do this before.

---
+++ install_osx.sh
@@ -0,0 +1,8 @@
+cp ./.vimrc ~/.vimrc
+mkdir -p ~/.vim/bundle/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
+echo "========================================================="
+echo "========================================================="
+echo "Finish this up by starting vim and running :BundleInstall"
+echo "========================================================="
+echo "========================================================="
